48c1dffcc8f020377cf32503f3eb6b2e28970aa3
Xander Gerrmann <xander@gerrmann.nl>
2020-11-12 23:37:48 +0100

    Add 2nd type of mortgage.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from enum import Enum
 
 import matplotlib.pyplot as plt
 import seaborn as sns
+
 import yaml
 
 settings_file = 'settings.yml'
@@ -30,10 +31,17 @@ class Mortgage:
         self._size = int(size)
         self._duration = int(duration)
         self._type = type
+        self._annuity = self._calc_annuity()
 
     def _calc_interest(self):
         # TODO should this be different than just a simple division by the amount of months?
         return self._size * self._rate / 12
+
+    def _calc_annuity(self):
+        # https: // www.hypotheekrente - annuiteitenhypotheek.nl / annuiteit - berekenen.php
+        n_periods = self._duration * 12
+        monthly_interest = self._rate / 12
+        return (monthly_interest / (1 - ((1 + monthly_interest) ** -n_periods))) * self._original_size
 
     def repay(self):
         if self._type == MortgageType.LINEAR:
@@ -41,8 +49,17 @@ class Mortgage:
             payoff = self._original_size / self._duration / 12
             self._size -= min(payoff, self._size)
             total_payment = payoff + interest
+        else:
+            interest = self._calc_interest()
+            payoff = self._annuity - interest
+            self._size -= min(payoff, self._size)
+            total_payment = self._annuity
 
         return total_payment, interest
+
+    @property
+    def size(self):
+        return self._size
 
 
 def calc_nett_payment(gross_payment, interest_payment):
@@ -54,7 +71,8 @@ def simulate(mortgage: Mortgage, settings):
     gross_payments = []
     nett_payments = []
     interest_payments = []
-    for ii in range(settings['simulation']['length']):
+    size = []
+    for ii in range(settings['simulation']['length']*12):
         gross_payment, interest_payment = mortgage.repay()
 
         gross_payments.append(gross_payment)
@@ -63,6 +81,7 @@ def simulate(mortgage: Mortgage, settings):
         nett_payment = calc_nett_payment(gross_payment, interest_payment)
         nett_payments.append(nett_payment)
 
+        size.append(mortgage._size)
 
     sns.set()
     print(gross_payments)
@@ -73,7 +92,10 @@ def simulate(mortgage: Mortgage, settings):
     plt.plot(nett_payments, label='nett')
     plt.plot(interest_payments, label='interest')
     plt.legend()
-    plt.ylim(0, max(gross_payments)*1.1)
+    plt.ylim(0, max(gross_payments) * 1.1)
+    plt.show()
+
+    plt.plot(size)
     plt.show()
 
 
